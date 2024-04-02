64d83b30fa254cf6271cec2b85b509ec00c68673
kylama09 <marieilarde@gmail.com>
2024-04-02 17:16:24 +0800

    first commit

---
+++ resources/views/post/index.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Baltazar</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="">
+    </head>
+    <body>
+        <h1>My Activity 6</h1>
+        <ul>
+            <li><a href="post/1">Post 1</a></li>
+            <li><a href="post/2">Post 2</a></li>
+        </ul>
+
+        <script src="" async defer></script>
+    </body>
+</html>
